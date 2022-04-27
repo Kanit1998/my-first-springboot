2f11a1b08b0218a8a133c5358c547476853f0139
Kanit1998 <102028193+Kanit1998@users.noreply.github.com>
2022-04-27 23:22:23 +0530

    " added three package serice ,serviceimpl, and dto and create one interface TestService from service package and create one class in serviceimpl TestServiceImpl from Getmapping and in dto i create one class StudentDetails from postmapping"

--- src/main/java/com/dlithe/bankingapp/controller/TestController.java
+++ src/main/java/com/dlithe/bankingapp/controller/TestController.java
@@ -1,15 +1,66 @@
 package com.dlithe.bankingapp.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dlithe.bankingapp.dto.StudentDetails;
+import com.dlithe.bankingapp.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
 
+ @Autowired
+    private TestService testService;
 
+    //path variables
+    @GetMapping("fetch-product-details/{productName}")
+    public String myFirstMethod(@PathVariable String productName) {
+        return testService.fetchProductDetails(productName);
 
+    }
 
-        @GetMapping("test")
-        public String myFirstMethod() {
-            return "Hey welcome to java";
+    @PostMapping("register-student")
+    public String registerNewStudent(@RequestBody StudentDetails studentDetails){
+        return testService.registerStudent(studentDetails);
     }
 }
+//    @GetMapping("test")
+//    public String myFirstMethod() {
+//        return "Hey welcome to java";
+//    }
+//
+//    @GetMapping("fetch-product-detail/{productName}")
+//    public String myFirstMethod(@PathVariable String productName) {
+//        switch (productName) {
+//            case "mobilePhones":
+//                return mobilePhoneDetails();
+//            case "cricketKit":
+//                return cricketKit();
+//            default:
+//                return "no product found";
+//        }
+//    }
+//
+//    public String mobilePhoneDetails() {
+//        return "here is the list of mobile phones";
+//    }
+//
+//    public String cricketKit() {
+//        return "limited stock avaiable";
+//    }
+//
+//    //path variables
+//    @GetMapping("add/{numOne}/{numTwo}")
+//    public int myFirstMethod(@PathVariable int numOne, @PathVariable int numTwo) {
+//        return numOne + numTwo;
+//    }
+//
+//      @GetMapping("multiple/{firstNum}/{secondNum}")
+//      public int myFirstMethod(@PathVariable int firstNum, @PathVariable int secondNum){
+//         return firstNum * secondNum;
+//   }
+//
+//
+//   @GetMapping("try/{numberFirst}/{numberSecond}")
+//    public int myMethod(@PathVariable int numberFirst, int numberSecond)
+
+
+
---
+++ src/main/java/com/dlithe/bankingapp/serviceimpl/TestServiceImpl.java
@@ -0,0 +1,34 @@
+package com.dlithe.bankingapp.serviceimpl;
+
+import com.dlithe.bankingapp.dto.StudentDetails;
+import com.dlithe.bankingapp.service.TestService;
+import org.springframework.stereotype.Service;
+
+    @Service
+    public class TestServiceImpl implements TestService{
+
+        @Override
+        public String fetchProductDetails(String productName){
+            if (productName!=null){
+                switch (productName){
+                    case "A":
+                        return "Best quality products";
+                    case "B":
+                        return "Good quality products";
+                    case "C":
+                        return "Low quality products";
+                    default:
+                        return "No product found";
+                }
+            }else {
+                return "product name can not be null";
+            }
+
+        }
+
+        @Override
+        public String registerStudent(StudentDetails studentDetailsRequest) {
+            return null;
+        }
+    }
+
